c104b7c42a10cab7fb8141108b117efebe7339bd
Patrick Wu <me@patrickwu.space>
2022-07-27 11:57:03 +0800

    fixing ancient building script with proper syntax

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-BUILD_DIR=`mktemp --tmpdir --directory tltk-build-debian.XXXX`
+BUILD_DIR=$(mktemp --tmpdir --directory tltk-build-debian.XXXX)
 BUILD_VER="0.7-0"
-CURRENT_DIR=`pwd`
+CURRENT_DIR=$(pwd)
 
-mkdir $BUILD_DIR/{DEBIAN/,usr/,usr/bin/}
+mkdir "$BUILD_DIR"/{DEBIAN/,usr/,usr/bin/}
 
-touch $BUILD_DIR/DEBIAN/control
+touch "$BUILD_DIR"/DEBIAN/control
 
-cat <<EOF >>$BUILD_DIR/DEBIAN/control
+cat <<EOF >>"$BUILD_DIR"/DEBIAN/control
 Package: tltk
 Architecture: all
 Maintainer: Patrick Wu <wotingwu@live.com>
@@ -18,18 +18,18 @@ Description: Tiny Little ToolKit
  This is a small collection of utilities by Patrick Wu using Python3. 
 EOF
 
-cp src/* $BUILD_DIR/usr/bin/
+cp src/* "$BUILD_DIR"/usr/bin/
 
-cd $BUILD_DIR
+cd "$BUILD_DIR" || exit
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
 
-find $BUILD_DIR -type d -exec chmod 0755 {} \;
-find $BUILD_DIR/usr/bin/ -type f -exec chmod 0555 {} \;
+find "$BUILD_DIR" -type d -exec chmod 0755 {} \;
+find "$BUILD_DIR"/usr/bin/ -type f -exec chmod 0555 {} \;
 
-[ -d $CURRENT_DIR/target ] || mkdir $CURRENT_DIR/target
-cd $CURRENT_DIR/target/
+[ -d "$CURRENT_DIR"/target ] || mkdir "$CURRENT_DIR"/target
+cd "$CURRENT_DIR"/target/ || exit
 
-sudo dpkg -b $BUILD_DIR/ tltk-${BUILD_VER}.deb
+sudo dpkg -b "$BUILD_DIR"/ tltk-${BUILD_VER}.deb
 
-rm -rf $BUILD_DIR
-cd $CURRENT_DIR
+rm -rf "$BUILD_DIR"
+cd "$CURRENT_DIR" || exit
